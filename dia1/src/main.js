a1f21b1aa8b2e0fc69820f59a7e09b4776dbd83f
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-10-30 13:39:22 -0300

    Exercícios resolvidos dia 1

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,13 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('[data-js="app"]');
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+`;
+
+document.querySelector('[data-js="link"]').addEventListener("click", (e) => {
+  e.preventDefault();
+  app.classList.toggle("hide");
+});
